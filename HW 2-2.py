236ff4737380a79c5d3b19cca39596ac85b2cce6
Cam Petrelli <p22cpetrelli@fairfieldprep.org>
2021-09-21 10:11:51 -0400

    add HW file

---
+++ HW 2-2.py
@@ -0,0 +1,7 @@
+
+freeThrowsScored = 2
+shortBasketsScored = 2
+longBasketsScored = 2
+
+totalpoints = (1 * freeThrowsScored) + (2 * shortBasketsScored) + (3 * longBasketsScored)
+print(totalpoints)
